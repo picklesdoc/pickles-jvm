99e3d2c1c62e07ffb9857387061ab848fe37f057
Jeffrey Cameron <jeffreycameron@gmail.com>
2012-09-22 11:33:56 -0400

    Forgot to implement the build method from the trait

--- src/main/scala/org/pickles/documentation/html/HtmlDocumentationBuilder.scala
+++ src/main/scala/org/pickles/documentation/html/HtmlDocumentationBuilder.scala
@@ -4,11 +4,15 @@
 package org.pickles.documentation.html
 
 import org.pickles.documentation.DocumentationBuilder
+import org.pickles.Configuration
+import org.pickles.crawler.TreeItem
 
 /**
  * @author jeffrey
  *
  */
 class HtmlDocumentationBuilder extends DocumentationBuilder {
+  def build(root: TreeItem, configuration: Configuration) = {
 
+  }
 }
