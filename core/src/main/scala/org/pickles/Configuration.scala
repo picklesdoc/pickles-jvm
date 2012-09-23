0ba1feeef2791c71ced518f9ee644f0690590a94
Jeffrey Cameron <jeffreycameron@gmail.com>
2012-09-23 08:22:00 -0400

    Added he documentationFormat and testResultsFormat fields to the configuration

--- core/src/main/scala/org/pickles/Configuration.scala
+++ core/src/main/scala/org/pickles/Configuration.scala
@@ -21,9 +21,9 @@ import org.apache.commons.vfs.FileObject
 class Configuration {
   var featureFolder: FileObject = null
   var outputFolder: FileObject = null
-  //def documentationFormat: DocumentationFormat = null
+  var documentationFormat: String = "HTML"
   var language: String = ""
-  //def testResultsFormat: TestResultsFormat = null
+  var testResultsFormat: String = null
   var hasTestResults: Boolean = testResultsFile.isDefined
   var testResultsFile: Option[FileObject] = None
   var systemUnderTestName: String = ""
